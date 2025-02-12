fac3f81dbd4a413343a001992abb20cf5f85d326
Andrey <pise4kan8@gmail.com>
2025-02-12 22:26:24 +0300

    updated tests

--- tests/test_category.py
+++ tests/test_category.py
@@ -1,50 +1,11 @@
-import pytest
-
-from src.category import Category
 from src.product import Product
-
-
-@pytest.fixture
-def category_1():
-    return Category(
-        name="Смартфоны",
-        description="Смартфоны, как средство не только коммуникации, "
-                    "но и получения дополнительных функций для удобства жизни",
-        products=[
-            Product("Samsung Galaxy S23 Ultra", "256GB, "
-                                                "Серый цвет, 200MP камера", 180000.0, 5),
-            Product("Iphone 15", "512GB, Gray space", 210000.0, 8),
-            Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
-        ]
-    )
-
-
-@pytest.fixture
-def category_2():
-    return Category(
-        name="Телевизоры",
-        description="Современный телевизор, "
-                    "который позволяет наслаждаться просмотром, станет вашим другом и помощником",
-        products=[
-            Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7)
-        ]
-    )
-
-
-@pytest.fixture
-def product_dict():
-    return {
-        "name": "Стиральная машина",
-        "description": "Описание Стиральной машины",
-        "price": 23450.00,
-        "quantity": 5,
-    }
 
 
 def test_classes_category(category_1, category_2):
     assert category_1.name == "Смартфоны"
-    assert category_1.description == ("Смартфоны, как средство не только коммуникации, "
-                                      "но и получения дополнительных функций для удобства жизни")
+    assert category_1.description == (
+        "Смартфоны, как средство не только коммуникации, " "но и получения дополнительных функций для удобства жизни"
+    )
     assert category_1.category_count == 2
     assert category_2.product_count == 4
 
@@ -57,19 +18,17 @@ def test_new_product(product_dict):
     assert product4.quantity == 5
 
 
-def test_add_product():
-    product = Product("Часы", "Механические, наручные", 30000.00, 20)
-    category = Category("Аксессуары", "Наручные", [])
-    category.add_product(product)
-    assert category._product_count == 1
+def test_add_product(product_1, category_3):
+    category_3.add_product(product_1)
+    assert category_3._product_count == 1
 
 
-def test_products_property():
-    product1 = Product("Ноутбук", "Ультратонкий", 150000.00, 10)
-    product2 = Product("Смартфон", "Iphone 16", 130000.00, 5)
+def test_products_property(product_1, product_2, category_3):
+    assert (
+        category_3.products == 'Название продукта 55" QLED 4K, 123000.0 руб. Остаток 7\n'
+        "Название продукта Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток 5\n"
+    )
 
-    category = Category("Электроника", "Гаджеты и девайсы", [product1, product2])
 
-    expected_output = ('Название продукта Ноутбук, 150000.0 руб. Остаток 10\n'
- 'Название продукта Смартфон, 130000.0 руб. Остаток 5\n')
-    assert category.products == expected_output
+def test_category_str(product_1, product_2, category_3):
+    assert str(category_3) == "Электроника, количество продуктов: 10 шт."
--- tests/test_product.py
+++ tests/test_product.py
@@ -1,32 +1,30 @@
-import pytest
-
-from src.product import Product
-
-
-@pytest.fixture
-def product():
-    return Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7)
+def test_classes_product(product_1):
+    assert product_1.name == "55\" QLED 4K"
+    assert product_1.description == "Фоновая подсветка"
+    assert product_1.price == 123000.0
+    assert product_1.quantity == 7
 
 
-def test_classes_product(product):
-    assert product.name == "55\" QLED 4K"
-    assert product.description == "Фоновая подсветка"
-    assert product.price == 123000.0
-    assert product.quantity == 7
+def test_price_setter(product_1):
+    product_1.price = 0
+    assert product_1.price == 123000.0
+
+    product_1.price = -120000
+    assert product_1.price == 123000.0
 
 
-def test_price_setter():
-    product = Product("55\" QLED 4K", "Фоновая подсветка", 123000.0, 7)
-    product.price = 0
-    assert product.price == 123000.0
+def test_valid_price_update(product_1):
+    assert product_1.price == 123000.0
 
-    product.price = -120000
-    assert product.price == 123000.0
+    product_1.price = 12000.00
+    assert product_1.price == 12000.00
 
 
-def test_valid_price_update():
-    product = Product("Клавиатура", "Механическая клавиатура", 10000.00, 30)
-    assert product.price == 10000.00
+def test_product_str(product_2):
+    assert str(product_2) == "Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 5 шт."
 
-    product.price = 12000.00
-    assert product.price == 12000.00
+
+def test_add_products(product_2, product_3):
+    total_value = product_2 + product_3
+    expected_value = (180000.0 * 5) + (31000.0 * 14)
+    assert total_value == expected_value
