631b99a902fb1745ba55cf295eaa751b28e74a32
Andrey <pise4kan8@gmail.com>
2025-02-12 22:25:10 +0300

    add conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,58 @@
+import pytest
+
+from src.category import Category
+from src.product import Product
+
+
+@pytest.fixture
+def product_1():
+    return Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)
+
+
+@pytest.fixture
+def product_2():
+    return Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+
+
+@pytest.fixture
+def product_3():
+    return Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+
+
+@pytest.fixture
+def category_1():
+    return Category(
+        name="Смартфоны",
+        description="Смартфоны, как средство не только коммуникации, "
+        "но и получения дополнительных функций для удобства жизни",
+        products=[
+            Product("Samsung Galaxy S23 Ultra", "256GB, " "Серый цвет, 200MP камера", 180000.0, 5),
+            Product("Iphone 15", "512GB, Gray space", 210000.0, 8),
+            Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14),
+        ],
+    )
+
+
+@pytest.fixture
+def category_2():
+    return Category(
+        name="Телевизоры",
+        description="Современный телевизор, "
+        "который позволяет наслаждаться просмотром, станет вашим другом и помощником",
+        products=[Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)],
+    )
+
+
+@pytest.fixture
+def category_3(product_1, product_2):
+    return Category("Электроника", "Гаджеты и девайсы", [product_1, product_2])
+
+
+@pytest.fixture
+def product_dict():
+    return {
+        "name": "Стиральная машина",
+        "description": "Описание Стиральной машины",
+        "price": 23450.00,
+        "quantity": 5,
+    }
